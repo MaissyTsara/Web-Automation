f34e24e6bd4fd3ca8f92a815640937136da626ec
MaissyTsara <maissypermata@gmail.com>
2025-03-04 02:46:17 +0700

    updated file

---
+++ webautomation/src/main/java/com/webautomation/locator/LocatorPractice.java
@@ -0,0 +1,26 @@
+package com.webautomation.locator;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LocatorPractice {
+
+    public static void main(String[] args) throws InterruptedException {
+        
+        //Setting chrome drivernya
+        System.setProperty("webdriver.chrome.driver", "D:/QA/COURSE/Learn After Office Bootcamp/Bootcamp Automation/Web Automation/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        //akses webnya
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        Thread.sleep(1000);       
+        
+        driver.close();
+        
+    }
+
+}
